8e3e4dad463bd6a87a8f7bb9aee523ffaa8514b2
Jean-David Flament <flamentjeandavid@yahoo.fr>
2018-01-03 18:43:09 +0100

    add bareme

--- sendmarks.php
+++ sendmarks.php
@@ -17,7 +17,10 @@ require 'vendor/autoload.php';
     <input type="file" id="file" name="file">
     <br />
     <label for="matiere">Indiquez une matière</label>
-    <input type="text" placeholder="Algo.." name="matiere">
+    <input type="text" placeholder="Exemple : Algorithmique" name="matiere">
+    <br />
+    <label for="bareme">Indiquez votre barême</label>
+    <input type="text" placeholder="Exemple : / 20" name="bareme">
     <input type="submit" value="Valider" name="submit">
 </form>
 
@@ -28,12 +31,14 @@ if(isset($_POST["submit"])) {
     if ( isset($_FILES["file"])) {
         $file = $_FILES["file"]["name"];
         $matiere = $_POST["matiere"];
+        $bareme = $_POST["bareme"];
         move_uploaded_file($_FILES["file"]["tmp_name"],$file);
         if (isset($file)) {
             $row = 1;
             $tab = array();
             echo '<form method="post">';
             echo '<input type="hidden" name="matiereValue" value="'.$matiere.'" />';            
+            echo '<input type="hidden" name="baremeValue" value="'.$bareme.'" />';            
             echo '<button id="sendmail" type="submit" name="sendMail">Envoyer les notes</button>';
             echo '<table border=1>';
             echo '<thead><tr><th>ID</th><th>Numéro Etudiant</th><th>Adresse mail</th><th>Note</th></thead>';
@@ -65,6 +70,7 @@ if(isset($_POST["submit"])) {
 
 if(isset($_POST["sendMail"])) {
     $matiere = $_POST["matiereValue"];
+    $bareme = $_POST["baremeValue"];
     foreach($_POST["tableau"] as $case){
         $mail = new PHPMailer;
         $mail->CharSet = 'UTF-8';
@@ -88,7 +94,7 @@ if(isset($_POST["sendMail"])) {
                                 <hr style="margin-top:10px;margin-bottom:65px;border:none;border-bottom:1px solid red;" />
                                 <h1 style="font-family: Tahoma, Geneva, sans-serif; font-weight: normal; color: #2A2A2A; text-align: center; margin-bottom: 65px;font-size: 20px; letter-spacing: 6px;font-weight: normal; border: 2px solid black; padding: 15px;">VOUS AVEZ UNE NOUVELLE NOTE !</h1>
                                 <h3 style="font-family:Palatino Linotype, Book Antiqua, Palatino, serif;font-style:italic;font-weight:500;">Note dans la matière <span style="border-bottom: 1px solid red;">' . $matiere . ' : ' . '</span></h3>
-                                <p style="text-align:center;font-family:Palatino Linotype, Book Antiqua, Palatino, serif;font-size: 18px; margin-left: auto; margin-right: auto;;color: #666;line-height:1.5;margin-bottom:75px;">'.$case["note"].' / 20</p>
+                                <p style="text-align:center;font-family:Palatino Linotype, Book Antiqua, Palatino, serif;font-size: 18px; margin-left: auto; margin-right: auto;;color: #666;line-height:1.5;margin-bottom:75px;">'.$case["note"]. ' '.$bareme.'</p>
                                 <p style="text-align:center;font-family:Palatino Linotype, Book Antiqua, Palatino, serif;font-size: 18px; margin-left: auto; margin-right: auto;;color: #666;line-height:1.5;margin-bottom:75px;">Merci de ne pas répondre à ce mail</p>
                                 <hr style="margin-top:10px;margin-top:75px;" />
                                 <p style="text-align:center;margin-bottom:15px;"><small style="text-align:center;font-family:Courier New, Courier, monospace;font-size:10px;color#666;">Fait avec <span style="color:red;">&hearts;</span> à Lens</small></p>
