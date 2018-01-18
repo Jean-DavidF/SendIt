1cde20b3d9e3f2ec8d0d814af8b0af289cfe80ab
Jean-David Flament <flamentjeandavid@yahoo.fr>
2018-01-18 21:38:11 +0100

    update list.php

--- list.php
+++ list.php
@@ -8,7 +8,7 @@
 
     <!-- Style -->
     <link rel="stylesheet" href="css/styles.css">
-    <link rel="stylesheet" href="css/sendtext.css">
+    <link rel="stylesheet" href="css/list.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato|Philosopher:400,400i,700,700i">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
